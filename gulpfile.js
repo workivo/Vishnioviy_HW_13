c6becf43135a4df507b6fc018a191eff9958990e
workivo <workivo@icloud.com>
2022-08-19 18:03:08 +0300

    hw/final/part_1

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const csso = require('gulp-csso');
+const rename = require('gulp-rename');
+const del = require('del');
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+
+gulp.task('scss', () => {
+    return gulp.src('./scss/**/*.scss')
+    .pipe(sass())
+    .pipe(postcss([
+        autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename({
+        suffix: ".min",
+      }))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('clean', () => {
+    return del('css')
+})
+
+gulp.watch('./scss/**/*.scss', gulp.series('scss'));
+gulp.task('start', gulp.series('clean','scss'));
